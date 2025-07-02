37f903577a6703af85a20e5368c9af4f175a9eb1
hretheum <eof@offline.pl>
2025-07-02 11:32:47 +0200

    Fix ES modules issue in Vercel Functions

--- api/test.ts
+++ api/test.ts
@@ -1,3 +1,5 @@
-export default function handler(req: any, res: any) {
+const handler = (req: any, res: any) => {
   res.status(200).json({ message: 'Hello from API!' });
-}
+};
+
+export default handler;
